3cca845cef5df0beeb42f238c03c6fbdc80a46ed
pelo-petek <pelda.petekkaya@std.yildiz.edu.tr>
2024-01-18 01:05:51 +0300

    header created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <header className="menu">
+        <h1>RİZGAR COMPANY</h1>
+        <ul>
+            <li>HOME</li>
+            <li>MENU</li>
+            <li>ABOUT</li>
+            <li>PRODUCTS</li>
+        </ul>
+    </header>
     </div>
   );
 }
 
-export default App;
+function Footer(){
+  return <footer class="footer">
+  <ul>
+      <li>rizgar company</li>
+      <li>created by Pelda Petekkaya</li>
+  </ul>
+</footer>
+}
+
+export default Header;
